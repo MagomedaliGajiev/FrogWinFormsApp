f8bffe7e91dcc472699ac5bcbc9530731bbe02cb
Magomedali Gajiev <mag198421@gmail.com>
2024-07-10 09:53:24 +0300

    Добавляем объекты с изображениями лягушек и листов

--- FrogWinFormsApp/MainForm.Designer.cs
+++ FrogWinFormsApp/MainForm.Designer.cs
@@ -28,18 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            leftPictureBox1 = new PictureBox();
+            leftPictureBox2 = new PictureBox();
+            leftPictureBox3 = new PictureBox();
+            leftPictureBox4 = new PictureBox();
+            rightPictureBox3 = new PictureBox();
+            rightPictureBox2 = new PictureBox();
+            rightPictureBox1 = new PictureBox();
+            emptyPictureBox1 = new PictureBox();
+            rightPictureBox4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)leftPictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)leftPictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)leftPictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)leftPictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rightPictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rightPictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rightPictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)emptyPictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rightPictureBox4).BeginInit();
             SuspendLayout();
+            // 
+            // leftPictureBox1
+            // 
+            leftPictureBox1.Image = Properties.Resources.frog1;
+            leftPictureBox1.Location = new Point(0, 0);
+            leftPictureBox1.Name = "leftPictureBox1";
+            leftPictureBox1.Size = new Size(110, 110);
+            leftPictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            leftPictureBox1.TabIndex = 0;
+            leftPictureBox1.TabStop = false;
+            // 
+            // leftPictureBox2
+            // 
+            leftPictureBox2.Image = Properties.Resources.frog1;
+            leftPictureBox2.Location = new Point(110, 0);
+            leftPictureBox2.Name = "leftPictureBox2";
+            leftPictureBox2.Size = new Size(110, 110);
+            leftPictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            leftPictureBox2.TabIndex = 1;
+            leftPictureBox2.TabStop = false;
+            // 
+            // leftPictureBox3
+            // 
+            leftPictureBox3.Image = Properties.Resources.frog1;
+            leftPictureBox3.Location = new Point(220, 0);
+            leftPictureBox3.Name = "leftPictureBox3";
+            leftPictureBox3.Size = new Size(110, 110);
+            leftPictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            leftPictureBox3.TabIndex = 2;
+            leftPictureBox3.TabStop = false;
+            // 
+            // leftPictureBox4
+            // 
+            leftPictureBox4.Image = Properties.Resources.frog1;
+            leftPictureBox4.Location = new Point(330, 0);
+            leftPictureBox4.Name = "leftPictureBox4";
+            leftPictureBox4.Size = new Size(110, 110);
+            leftPictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            leftPictureBox4.TabIndex = 3;
+            leftPictureBox4.TabStop = false;
+            // 
+            // rightPictureBox3
+            // 
+            rightPictureBox3.Image = Properties.Resources.frog3;
+            rightPictureBox3.Location = new Point(770, 0);
+            rightPictureBox3.Name = "rightPictureBox3";
+            rightPictureBox3.Size = new Size(110, 110);
+            rightPictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            rightPictureBox3.TabIndex = 7;
+            rightPictureBox3.TabStop = false;
+            // 
+            // rightPictureBox2
+            // 
+            rightPictureBox2.Image = Properties.Resources.frog3;
+            rightPictureBox2.Location = new Point(660, 0);
+            rightPictureBox2.Name = "rightPictureBox2";
+            rightPictureBox2.Size = new Size(110, 110);
+            rightPictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            rightPictureBox2.TabIndex = 6;
+            rightPictureBox2.TabStop = false;
+            // 
+            // rightPictureBox1
+            // 
+            rightPictureBox1.Image = Properties.Resources.frog3;
+            rightPictureBox1.Location = new Point(550, 0);
+            rightPictureBox1.Name = "rightPictureBox1";
+            rightPictureBox1.Size = new Size(110, 110);
+            rightPictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            rightPictureBox1.TabIndex = 5;
+            rightPictureBox1.TabStop = false;
+            // 
+            // emptyPictureBox1
+            // 
+            emptyPictureBox1.Image = Properties.Resources.list;
+            emptyPictureBox1.Location = new Point(440, 0);
+            emptyPictureBox1.Name = "emptyPictureBox1";
+            emptyPictureBox1.Size = new Size(110, 110);
+            emptyPictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            emptyPictureBox1.TabIndex = 4;
+            emptyPictureBox1.TabStop = false;
+            // 
+            // rightPictureBox4
+            // 
+            rightPictureBox4.Image = Properties.Resources.frog3;
+            rightPictureBox4.Location = new Point(880, 0);
+            rightPictureBox4.Name = "rightPictureBox4";
+            rightPictureBox4.Size = new Size(110, 110);
+            rightPictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            rightPictureBox4.TabIndex = 8;
+            rightPictureBox4.TabStop = false;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(992, 178);
+            Controls.Add(rightPictureBox4);
+            Controls.Add(rightPictureBox3);
+            Controls.Add(rightPictureBox2);
+            Controls.Add(rightPictureBox1);
+            Controls.Add(emptyPictureBox1);
+            Controls.Add(leftPictureBox4);
+            Controls.Add(leftPictureBox3);
+            Controls.Add(leftPictureBox2);
+            Controls.Add(leftPictureBox1);
             Name = "MainForm";
             Text = "Frogs";
+            ((System.ComponentModel.ISupportInitialize)leftPictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)leftPictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)leftPictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)leftPictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rightPictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rightPictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rightPictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)emptyPictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rightPictureBox4).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox leftPictureBox1;
+        private PictureBox leftPictureBox2;
+        private PictureBox leftPictureBox3;
+        private PictureBox leftPictureBox4;
+        private PictureBox rightPictureBox3;
+        private PictureBox rightPictureBox2;
+        private PictureBox rightPictureBox1;
+        private PictureBox emptyPictureBox1;
+        private PictureBox rightPictureBox4;
     }
 }
